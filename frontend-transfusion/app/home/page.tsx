ce7a4069ddfacbc7ef40c2d8e19f23a9c0950219
Violeta <v.marceta@vegait.rs>
2023-09-06 15:39:57 +0200

    feature(frontend): restyle components and add some button interactivity

--- frontend-transfusion/app/home/page.tsx
+++ frontend-transfusion/app/home/page.tsx
@@ -1,26 +1,20 @@
-"use client"
-import Image from 'next/image'
 import Button from '@/components/PrimaryButton'
 import SecondaryButton from '@/components/SecondaryButton'
 import DisabledButton from '@/components/DisabledButton'
 import Card from '@/components/Card'
-import { useRouter } from 'next/navigation'
+
 
 export default function Page() {
-    const router = useRouter();
-    const handleBack = () => router.push("/");
-    const mockCentres = [{name: "centre 21", description: "some short description"}, {name: "centre 100", description: "some short description"}, {name: "centre 500", description: "some short description"}]
+    const mockCentres = [{name: "centre 21", description: "some short description some short description some short description some short description"}, {name: "centre 100", description: "some short description"}, {name: "centre 500", description: "some short description"}]
   
     return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
- 
-
-      <div className="relative flex flex-col place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      <div className="relative flex flex-col gap-y-20 place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
        <h2 className={`mb-3 text-2xl font-semibold`}>Transfusion centres</h2>
-       {!!mockCentres.length && mockCentres.map((centre) => <Card key={centre.name} name={centre.name} description={centre.description}/>)}
+       <div className="flex gap-5 z-10 items-stretch flex-wrap">
+       {!!mockCentres.length && mockCentres.map((centre) => <Card key={centre.name} name={centre.name} description={centre.description}/>)}</div>
        <div className="flex flex-row p-4">
-       
-       <Button title="back"  event={handleBack}/>
+       <Button title="back" />
        <SecondaryButton title='secondary'/>
        <DisabledButton title='disabled'/>
 </div>
--- frontend-transfusion/components/PrimaryButton.tsx
+++ frontend-transfusion/components/PrimaryButton.tsx
@@ -1,11 +1,12 @@
 "use client"
-import { MouseEventHandler } from "react";
+import { useRouter } from "next/navigation";
 
 
-export default function Button({title, event}: {title: string, event: MouseEventHandler<HTMLButtonElement> | undefined}) {
-   
+export default function Button({title}: {title: string}) {
+   const router = useRouter();
+   const handleBack = () => router.push("/");
   return (
-      <button onClick={event} className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+      <button onClick={() => {handleBack()}} className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
         {title}
       </button>
   );
